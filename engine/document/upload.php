14ca3e86ce64a3c0a5c324a2bc4dbb9d01b81394
lafleurdeboum <lafleur@boum.org>
2020-01-14 04:06:08 +0100

    try to clean if copy or DB insert fail on upload

--- engine/document/upload.php
+++ engine/document/upload.php
@@ -79,12 +79,12 @@ if(is_file($pool . "/" . $uploadFile)) {
   array_push($messages, array("Le fichier $uploadFile existe déjà", "alert-danger"));
   $title = "";
 } else {
-  if (move_uploaded_file($_FILES['filename']['tmp_name'], $pool . "/" . $uploadFile)) {
-    $type = mime_content_type($pool . "/" . $uploadFile);
-    $result = $db->exec(
-        "INSERT OR REPLACE INTO documents (filename, filetype, attached_notes) VALUES ('$uploadFile', '$type', ',$title,');"
-    );
-    if ($result) { 
+  $type = mime_content_type($pool . "/" . $uploadFile);
+  $filenameInserted = $db->exec(
+      "INSERT OR REPLACE INTO documents (filename, filetype, attached_notes) VALUES ('$uploadFile', '$type', ',$title,');"
+  );
+  if($filenameInserted) { 
+    if(move_uploaded_file($_FILES['filename']['tmp_name'], $pool . "/" . $uploadFile)) {
       array_push($messages, array("Fichier <b>$uploadFile</b> ajouté", "alert-success"));
       if(explode("/", $type)[0] == "image") {
         try {
@@ -103,10 +103,14 @@ if(is_file($pool . "/" . $uploadFile)) {
         }
       }
     } else {
-      array_push($messages, array("Le fichier <b>$uploadFile</b> n'a pas pu être ajouté à la base de données", "alert-danger"));
+      array_push($messages, array("Le fichier <b>$uploadFile</b> n'a pas pu être copié. Vérifiez les permissions sur le dossier <b>$pool</b> dans le serveur", "alert-danger"));
+      if($filenameInserted) {
+        $db->exec("DELETE FROM documents WHERE filename = '$uploadFile'");
+      }
+      // TODO We should tell a calling process like createFromDoc that this failed.
     }
   } else {
-    array_push($messages, array("Le fichier <b>$uploadFile</b> n'a pas pu être copié dans <b>$pool</b>. Vérifiez les permissions sur le dossier", "alert-danger"));
+    array_push($messages, array("Le fichier <b>$uploadFile</b> n'a pas pu être ajouté à la base de données", "alert-danger"));
   }
 }
 
